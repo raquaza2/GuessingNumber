697c58f14d01f306b85d8e28d781dec60ed84f04
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-04 00:08:22 +0800

    Complete
    Complete

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,11 +1,27 @@
 import random
 
 print("Enter a random number from 1 - 100, but you got only 3 changes.")
+print("Enter a random number from 1 - 5, but you got only 3 changes.")
 
 numToGuess = random.randrange(100)
+numToGuess = random.randrange(5)
 
 changes = 3
 
 guessCounter = 0
 
 while guessCounter < changes:
+    guessCounter += 1
+    playerGuess = int(input('Please enter your guess: '))
+    
+    if playerGuess == numToGuess:
+        print(f'Your guess is right in only {guessCounter}')
+        break
+    elif guessCounter >= changes and playerGuess != numToGuess:
+        print(f'You ran out of guesses. The number was {numToGuess}.')
+
+    elif playerGuess > numToGuess:
+        print('Your guess is too high')
+
+    elif playerGuess < numToGuess:
+        print('Your guess is too low')
